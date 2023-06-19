20ba271d20ad70ee5077460ac3b8539cff0e1b42
Jessy St-Pierre <jessy.stpierre@live.com>
2023-06-19 16:27:02 -0400

    Added Image component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
 import CardText from './components/card-text/CardText'
+import Image from './components/image/Image'
+import Bulbasaur from './assets/bulbasaur.png'
 
 function App() {
   
 
   return (
     <>
-      <CardText className="" as="">Hello World</CardText>
+      <Image
+        className="bulbasaur"
+        src={Bulbasaur}
+        alt="Bulbasaur"
+        width="100px"
+        height=""
+      
+      />
+      <CardText className="" as="">Bulbasaur</CardText>
     </>
   )
 }
