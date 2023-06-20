a6a230374db154e7ea949db98f92dc2c656cdbd2
Jessy St-Pierre <jessy.stpierre@live.com>
2023-06-20 18:06:31 -0400

    Added CardCollection component and finishing project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
-import CardText from './components/card-text/CardText'
-import Image from './components/image/Image'
-import Bulbasaur from './assets/bulbasaur.png'
+import CardCollection from './components/card-collection/CardCollection'
 import Card from './components/card/Card'
+
+
 function App() {
   
 
   return (
     <>
-      <Card></Card>
+      <CardCollection></CardCollection>
     </>
   )
 }
