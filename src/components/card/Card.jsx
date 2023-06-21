febdeb5417398873551a221ab6bc6a597326bd35
Jessy St-Pierre <jessy.stpierre@live.com>
2023-06-20 21:15:00 -0400

    removed comments

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,7 +7,6 @@ import { useState } from 'react'
 
 const Card = (props) => {
     const classnames = ['card'];
-    /* const abilityClassNames = []; */
     const [isActive, setIsActive] = useState("hidden");
 
     if (props.className) {
@@ -64,29 +63,3 @@ const Card = (props) => {
 }
 
 export default Card
-
-{/* <div className={classnames.join(" ")} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-            <PokemonName pokemonName="Bulbasaur"></PokemonName>
-            <Image
-                className="card-background"
-                src={CardBackground}
-                alt="Card Background"
-                width="420px"
-                height="590px"
-            />
-
-            <Image
-                className="pokemon-image"
-                src={Bulbasaur}
-                alt="Bulbasaur"
-                width="355px"
-                height=""
-            />
-            <div className={isActive || "hidden"}>
-                <Ability 
-                    abilityName1="Razor Leaf"
-                    abilityDmg1="30"
-                    abilityName2="Solar Beam"
-                    abilityDmg2="60"
-                ></Ability>
-            </div> */}
